c24ddbac8fbb37f6f1500deb90b283d40d21cc67
sahakyandav <david.sahakyan.work@gmail.com>
2022-03-11 14:59:51 +0400

    added scroller functionality

--- index.php
+++ index.php
@@ -20,19 +20,33 @@
         </div>
     </header>
     <div class="main-intro">
-        <div id="main-block1">
-            <p id='intro'></p>
+        <div class="main-texts1">
+            <div id="main-block1">
+                <p id='intro'></p>
+            </div>
+            <p id="main-block3">
+                <span id='intro3'></span>
+            </p>
         </div>
-        <!-- <div id="main-block2">
-            <p id='intro2'></p>
-        </div> -->
-        <p id="main-block3">
-            <span id='intro3'></span>
-        </p>
+        <section id="scroller1">
+            <a href="#2" class='scroll'>
+                <span></span><span></span><span></span>
+                <h1>Click to continue</h1>
+            </a>
+        </section>
     </div>
+    <div class="nor" style='height: 100vh'></div>
+    <div class="nor2" style='height: 100vh'></div>
     <script src="./js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
     <script>
+        let scroller1 = document.querySelector('#scroller1');
+        
+        if (history.scrollRestoration) {
+            history.scrollRestoration = 'manual';
+        }
+        window.scrollTo(0,0)
+
         var typed = new Typed('#intro', {
             strings: ['Hey', 'I\'m David'],
             typeSpeed: 90,
@@ -47,33 +61,27 @@
             }
         });
 
-        // setTimeout(() => {
-        //     var typed2 = new Typed('#intro2', {
-        //     strings: ['I\'m David'],
-        //     typeSpeed: 90,
-        //     onComplete: function() {
-        //         setTimeout(() => {
-        //             typed2.toggleBlinking();
-        //             typed2.cursor.parentElement.style.width = getComputedStyle(typed2.cursor.parentElement).getPropertyValue('width');
-        //             typed2.cursor.remove();
-        //         }, 40);
-        //     }
-        // });
-        // }, 750);
-
         setTimeout(() => {
             var typed2 = new Typed('#intro3', {
             strings: ['A back-end developer based in Yerevan, Armenia'],
             typeSpeed: 90,
             onComplete: function() {
                 setTimeout(() => {
-                    // typed2.toggleBlinking();
-                    // typed2.cursor.parentElement.style.width = '300px';
-                    // typed2.cursor.remove();
+                    scroller1.style.opacity = '1';
+                    scroller1.style.pointerEvents = 'auto';
                 }, 40);
             }
+        })}, 2400);
+
+        document.querySelectorAll('.scroll').forEach(btn => {
+            btn.addEventListener('click', function() {
+                window.scrollBy({
+                    left: 0,
+                    top: document.documentElement.clientHeight,
+                    behavior: 'smooth'
+                });
+            });
         });
-        }, 2400);
     </script>
 </body>
 
