62d5685c2595634c962aeaefd8d010442ed94716
sahakyandav <david.sahakyan.work@gmail.com>
2022-03-11 18:16:02 +0400

    semi-finished home page

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>davsahakyan</title>
     <link rel="stylesheet" href="./style/style.css">
+    <style>
+        body {
+            overflow-y: hidden;
+        }
+
+        #header {
+            height: 10vh;
+        }
+
+        @media screen and (max-width: 600px) {
+            #header {
+                height: 17vh;
+            }
+
+            .main-intro {
+                height: 77vh;
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -29,62 +48,27 @@
             </p>
         </div>
         <section id="scroller1">
-            <a href="#2" class='scroll'>
+            <a class='scroll'>
                 <span></span><span></span><span></span>
                 <h1>Click to continue</h1>
             </a>
         </section>
     </div>
-    <div class="nor" style='height: 100vh'></div>
-    <div class="nor2" style='height: 100vh'></div>
+    <div class="main-slide1">
+        <h1>A short <i>about me</i></h1>
+        <p class='main-story'>
+            <span id="storyline"></span>
+        </p>
+        <div class="main-languages">
+            <div class="language" id="l1"></div>
+            <div class="language" id="l2"></div>
+        </div>
+        <h2 id="outro">For more info on me, check <a href='./projects.php'><i>Projects</i></a> page</h2>
+    </div>
     <script src="./js/script.js"></script>
     <script src="./js/universalsmoothscroll-min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
-    <script>
-        let scroller1 = document.querySelector('#scroller1');
-        
-        if (history.scrollRestoration) {
-            history.scrollRestoration = 'manual';
-        }
-        window.scrollTo(0,0)
-
-        var typed = new Typed('#intro', {
-            strings: ['Hey', 'I\'m David'],
-            typeSpeed: 90,
-            backSpeed: 50,
-            backDelay: 300,
-            onComplete: function() {
-                setTimeout(() => {
-                    typed.toggleBlinking();
-                    typed.cursor.parentElement.style.width = getComputedStyle(typed.cursor.parentElement).getPropertyValue('width');
-                    typed.cursor.remove();
-                }, 40);
-            }
-        });
-
-        setTimeout(() => {
-            var typed2 = new Typed('#intro3', {
-            strings: ['A back-end developer based in Yerevan, Armenia'],
-            typeSpeed: 90,
-            onComplete: function() {
-                setTimeout(() => {
-                    scroller1.style.opacity = '1';
-                    scroller1.style.pointerEvents = 'auto';
-                }, 40);
-            }
-        })}, 2400);
-
-        document.querySelectorAll('.scroll').forEach(btn => {
-            btn.addEventListener('click', function() {
-                // window.scrollBy({
-                    // left: 0,
-                    // top: document.documentElement.clientHeight,
-                    // behavior: 'smooth'
-                // });
-                uss.scrollYBy(781)
-            });
-        });
-    </script>
+    <script src="./js/main.js"></script>
 </body>
 
 </html>
---
+++ js/main.js
@@ -0,0 +1,57 @@
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0)
+
+var typed = new Typed('#intro', {
+    strings: ['Hey', 'I\'m David'],
+    typeSpeed: 90,
+    backSpeed: 50,
+    backDelay: 300,
+    onComplete: function () {
+        setTimeout(() => {
+            typed.toggleBlinking();
+            typed.cursor.parentElement.style.width = getComputedStyle(typed.cursor.parentElement).getPropertyValue('width');
+            typed.cursor.remove();
+        }, 40);
+    }
+});
+
+let scroller1 = document.querySelector('#scroller1');
+setTimeout(() => {
+    new Typed('#intro3', {
+        strings: ['A starting back-end developer based in<br>Yerevan, Armenia'],
+        typeSpeed: 85,
+        onComplete: function () {
+            setTimeout(() => {
+                scroller1.style.opacity = '1';
+                scroller1.style.pointerEvents = 'auto';
+            }, 40);
+        }
+    })
+}, 2400);
+
+document.querySelectorAll('.scroll').forEach(btn => {
+    btn.addEventListener('click', function () {
+        uss.scrollYBy(document.documentElement.clientHeight)
+        setTimeout(() => {
+            var typed3 = new Typed('#storyline', {
+                strings: ['In 2021, I started my first full-time job as a developer', 'Learining how to create web banners, I started freelancing on side', 'But then it finally clicked - front-end was not for me,<br>so I started getting into back-end', 'Currently I am learning'],
+                typeSpeed: 70,
+                backSpeed: 8,
+                backDelay: 400,
+                onComplete: function () {
+                    setTimeout(() => {
+                        document.body.style.overflowY = 'initial';
+                        document.querySelectorAll('.language').forEach((lang, i) => {
+                            lang.style.animation = `language 0.9s ease ${i * 0.6}s forwards`;
+                        })
+                        setTimeout(() => { 
+                            document.querySelector('#outro').style.animation = `outro 1s ease forwards`;
+                        }, 1550)
+                    }, 40);
+                }
+            })
+        }, 800);
+    });
+});
