41687239454326018b4e97c7ded2bb9dce32f5df
sahakyandav <david.sahakyan.work@gmail.com>
2022-03-18 11:59:33 +0400

    cosmetics

--- projects.php
+++ projects.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>davsahakyan</title>
     <link rel="stylesheet" href="./style/style.css">
-    <link rel="stylesheet" href="./style/projects.css">
     <style>
  
 
